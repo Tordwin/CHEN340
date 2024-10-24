abb95db4159f73dc4bc5a1774be6614b65e2282c
Tordwin <ec7233@g.rit.edu>
2024-10-24 10:20:59 -0400

    react

---
+++ react/vite-project/components/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title(obj){
+    return(
+        <>
+            <h1>{myObj.title}</h1>
+            <h5>{myObj.description}</h5>
+        </>
+    )
+}
---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,46 @@
+//load my stuff
+//import React from 'react' (instead of using this use below)
+//if you use the above you will have to do react.function()
+
+import { useState } from 'react'
+
+// load my comp
+import Welcome from '/components/functionalComp'
+import InLineComp from './components/otherComp'
+import Title from './components/title'
+
+import './App.css'
+
+function App(){
+    //writing my (App) private vars
+    //state
+    //const {getter, setter} = userState(init);
+    //flag for if it is loaded yet
+    const [loaded, setLoaded] = useState(false);
+    //obj to hold my returned data
+    const [obj, setMyObj] = useState();
+    
+    //write my (App) methods
+    //let's go faux the date...
+    const getData = () =>{
+        //usuallly we would go out and load data!
+        //today we are faking it to see useEffect and useState working
+        setMyObj({
+            title:"React is fun!",
+            description:"lots and lots of words, something more meaningful than this, yadda, yadda..."
+        });
+        setLoaded(true);
+    }
+
+    if (!loaded) return (<>Loading... <button/></>)
+
+    return (
+        <>
+            <Title obj = {myObj}/>
+            <h1>{myObj.title}</h1>
+            <p>{myObj.description}</p>
+            <InLineComp name="fred" age="53"/>
+            <Welcome name="dan" job="talk"/>
+        </>
+    );
+}
